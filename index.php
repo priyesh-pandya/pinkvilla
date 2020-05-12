16c829c5c2d736da11851997270d2571e60688cf
Priyesh <priyesh.ak38@gmail.com>
2020-05-12 10:27:34 +0530

    added index.php home page

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html class="gr__getbootstrap_com" lang="en"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <title>Priyesh Pandya</title>     
+   <!-- Bootstrap core CSS --> 
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<!-- Favicons -->
+<link rel="icon" href="https://www.pinkvilla.com/files/icons/favicon.ico">
+<meta name="theme-color" content="#563d7c">
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+  <style> .seo-highlighter { border-color: red; border-width: 5px; border-style: solid; border-radius: 18px; }</style></head>
+  <body class="text-center" data-gr-c-s-loaded="true" style="background-color: #000000; color: #ffffff;">
+  
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="masthead mb-auto">
+    <div class="inner">
+      <h3 class="masthead-brand">Pinkvilla</h3> 
+    </div>
+  </header>
+  
+  <div style='margin:50px'></div>
+
+  <main role="main" class="inner cover">
+    <h1 class="cover-heading">Click below to access webpages</h1>
+ <div>
+
+    <p class="lead">
+      <a href="webpage1.php" class="btn btn-lg btn-secondary">PHP Webpage-1</a>
+    </p>
+	
+	<p class="lead">
+      <a href="webpage2.html" class="btn btn-lg btn-secondary">HTML Webpage-2</a>
+    </p>
+ </div>
+ 
+  </main>
+
+   <div style='margin:50px'></div>
+ 
+  <footer class="mastfoot mt-auto">
+    <div class="inner">
+	    <p class="lead">There are no secrets to success. It is the result of preparation, hard work, and learning from failure.
+
+      <p>  © 2020 All rights reserved </p>
+    </div>
+  </footer>
+
+</div>
+</body>
+</html>
+
+<div class='container' style="display:none" >
+<?php 
+ echo phpinfo();
+?>
+</div>
